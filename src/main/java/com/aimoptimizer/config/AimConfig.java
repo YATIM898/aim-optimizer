e1ebf6e0a83d4eb03ae82c4a8617f938e8d411eb
YATIM898 <oggodsog@gmail.com>
2026-08-13 08:54:24 +0530

    Create AimConfig.java

---
+++ src/main/java/com/aimoptimizer/config/AimConfig.java
@@ -0,0 +1,8 @@
+package com.aimoptimizer.config;
+
+public class AimConfig {
+    public volatile boolean enabled = true;
+    public volatile double alpha = 0.30;
+    public volatile double bypassThreshold = 8.0;
+    public volatile double smoothedDY = 0.0;
+}
